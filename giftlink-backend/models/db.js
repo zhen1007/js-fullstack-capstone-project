d16828ba0b9ee6245ec58616120e68d2aae4db94
zhen1007 <zhen1007@outlook.com>
2024-09-29 13:22:20 -0400

    implement /api/gifts

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,6 +1,6 @@
 // db.js
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
 
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
@@ -9,17 +9,17 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
-
-    const client = new MongoClient(url);      
-
-    await client.connect();
-
-    dbInstance = client.db(dbName);
-
+  if (dbInstance) {
     return dbInstance;
+  }
+
+  const client = new MongoClient(url);
+
+  await client.connect();
+
+  dbInstance = client.db(dbName);
+
+  return dbInstance;
 }
 
 module.exports = connectToDatabase;
