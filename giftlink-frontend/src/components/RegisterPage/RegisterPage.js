5b8f0c5a4a7d6d39bfa2d72937e1c4297f9fcbd0
zhen1007 <zhen1007@outlook.com>
2024-09-29 16:50:08 -0400

    Implement Back-end in Register Component

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-
+import { urlConfig } from "../../config";
+import { useAppContext } from "../../context/AuthContext.js";
+import { useNavigate } from "react-router-dom";
 import "./RegisterPage.css";
 
 function RegisterPage() {
@@ -7,9 +9,44 @@ function RegisterPage() {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showerr, setShowerr] = useState("");
+
+  const navigate = useNavigate();
+  const { setIsLoggedIn } = useAppContext();
 
   const handleRegister = async () => {
-    console.log("Register invoked");
+    try {
+      const response = await fetch(
+        `${urlConfig.backendUrl}/api/auth/register`,
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+          }),
+        }
+      );
+
+      const json = await response.json();
+
+      if (json.authtoken) {
+        sessionStorage.setItem("auth-token", json.authtoken);
+        sessionStorage.setItem("name", firstName);
+        sessionStorage.setItem("email", json.email);
+        setIsLoggedIn(true);
+        navigate("/app");
+      }
+      if (json.error) {
+        setShowerr(json.error);
+      }
+    } catch (e) {
+      console.log("Error fetching details: " + e.message);
+    }
   };
 
   return (
@@ -79,6 +116,7 @@ function RegisterPage() {
             >
               Register
             </button>
+            <div className="text-danger">{showerr}</div>
             <p className="mt-4 text-center">
               Already a member?{" "}
               <a href="/app/login" className="text-primary">
