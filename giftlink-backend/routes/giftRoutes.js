60144909ec0c587d761bf51f40153efbe141d908
zhen1007 <zhen1007@outlook.com>
2024-09-29 13:23:36 -0400

    implement the /api/gifts/:id

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -17,16 +17,13 @@ router.get("/", async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   try {
-    // Task 1: Connect to MongoDB and store connection to db constant
-    // const db = {{insert code here}}
+    const db = await connectToDatabase();
 
-    // Task 2: use the collection() method to retrieve the gift collection
-    // {{insert code here}}
+    const collection = db.collection("gifts");
 
     const id = req.params.id;
 
-    // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-    // {{insert code here}}
+    const gift = await collection.findOne({ id: id });
 
     if (!gift) {
       return res.status(404).send("Gift not found");
