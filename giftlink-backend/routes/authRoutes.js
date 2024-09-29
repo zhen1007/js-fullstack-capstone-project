d85e791f503747dffccd86097186ee0294ac03b9
zhen1007 <zhen1007@outlook.com>
2024-09-29 17:07:15 -0400

    Develop Back-end Integration for User Profile

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -6,19 +6,17 @@ const router = express.Router();
 const dotenv = require("dotenv");
 const pino = require("pino"); // Import Pino logger
 
+const { body, validationResult } = require("express-validator");
+
 const logger = pino(); // Create a Pino logger instance
 
 dotenv.config();
-
-//Create JWT secret
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post("/register", async (req, res) => {
   try {
     const db = await connectToDatabase();
-
     const collection = db.collection("users");
-
     const existingEmail = await collection.findOne({ email: req.body.email });
 
     if (existingEmail) {
@@ -29,7 +27,7 @@ router.post("/register", async (req, res) => {
     const salt = await bcryptjs.genSalt(10);
     const hash = await bcryptjs.hash(req.body.password, salt);
     const email = req.body.email;
-
+    console.log("email is", email);
     const newUser = await collection.insertOne({
       email: req.body.email,
       firstName: req.body.firstName,
@@ -89,6 +87,60 @@ router.post("/login", async (req, res) => {
     return res
       .status(500)
       .json({ error: "Internal server error", details: e.message });
+  }
+});
+
+// update API
+router.put("/update", async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    logger.error("Validation errors in update request", errors.array());
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const email = req.headers.email;
+
+    if (!email) {
+      logger.error("Email not found in the request headers");
+      return res
+        .status(400)
+        .json({ error: "Email not found in the request headers" });
+    }
+
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
+
+    const existingUser = await collection.findOne({ email });
+
+    if (!existingUser) {
+      logger.error("User not found");
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    existingUser.firstName = req.body.name;
+    existingUser.updatedAt = new Date();
+
+    const updatedUser = await collection.findOneAndUpdate(
+      { email },
+      { $set: existingUser },
+      { returnDocument: "after" }
+    );
+
+    const payload = {
+      user: {
+        id: updatedUser._id.toString(),
+      },
+    };
+
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+    logger.info("User updated successfully");
+
+    res.json({ authtoken });
+  } catch (error) {
+    logger.error(error);
+    return res.status(500).send("Internal Server Error");
   }
 });
 
