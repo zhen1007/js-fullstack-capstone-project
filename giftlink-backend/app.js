fd911b03059a08f60bcc06fe35b43a98bacb8463
zhen1007 <zhen1007@outlook.com>
2024-09-29 13:48:15 -0400

    Integrate search routes in app.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -24,8 +24,7 @@ app.use(express.json());
 // Route files
 const giftRoutes = require('./routes/giftRoutes');
 
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+const searchRoutes = require('./routes/searchRoutes');
 
 
 const pinoHttp = require('pino-http');
@@ -36,8 +35,7 @@ app.use(pinoHttp({ logger }));
 // Use Routes
 app.use('/api/gifts', giftRoutes);
 
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/search', searchRoutes);
 
 
 // Global Error Handler
