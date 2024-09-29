d59140941c7f8b6d03f86003290aa3a8b0ccdfb9
zhen1007 <zhen1007@outlook.com>
2024-09-29 17:16:04 -0400

    Implement Back-end in Profile Component

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -5,6 +5,7 @@ import LoginPage from "./components/LoginPage/LoginPage";
 import RegisterPage from "./components/RegisterPage/RegisterPage";
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
@@ -20,6 +21,7 @@ function App() {
         <Route path="/app/register" element={<RegisterPage />} />
         <Route path="/app/product/:productId" element={<DetailsPage/>} />
         <Route path="/app/search" element={<SearchPage/>} />
+        <Route path="/app/profile" element={<Profile/>} />
       </Routes>
     </>
   );
