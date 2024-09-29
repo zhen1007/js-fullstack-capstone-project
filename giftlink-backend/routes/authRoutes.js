b90a92e43f290086435ecc6751a8105c4b1313b4
zhen1007 <zhen1007@outlook.com>
2024-09-29 17:01:25 -0400

    Develop Back-end Integration for Login

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const app = express();
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { body, validationResult } = require("express-validator");
 const connectToDatabase = require("../models/db");
 const router = express.Router();
 const dotenv = require("dotenv");
@@ -12,6 +10,7 @@ const logger = pino(); // Create a Pino logger instance
 
 dotenv.config();
 
+//Create JWT secret
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post("/register", async (req, res) => {
@@ -21,6 +20,11 @@ router.post("/register", async (req, res) => {
     const collection = db.collection("users");
 
     const existingEmail = await collection.findOne({ email: req.body.email });
+
+    if (existingEmail) {
+      logger.error("Email id already exists");
+      return res.status(400).json({ error: "Email id already exists" });
+    }
 
     const salt = await bcryptjs.genSalt(10);
     const hash = await bcryptjs.hash(req.body.password, salt);
@@ -44,7 +48,47 @@ router.post("/register", async (req, res) => {
     logger.info("User registered successfully");
     res.json({ authtoken, email });
   } catch (e) {
+    logger.error(e);
     return res.status(500).send("Internal server error");
+  }
+});
+
+//Login Endpoint
+router.post("/login", async (req, res) => {
+  console.log("\n\n Inside login");
+
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
+    const theUser = await collection.findOne({ email: req.body.email });
+
+    if (theUser) {
+      let result = await bcryptjs.compare(req.body.password, theUser.password);
+      if (!result) {
+        logger.error("Passwords do not match");
+        return res.status(404).json({ error: "Wrong pasword" });
+      }
+      let payload = {
+        user: {
+          id: theUser._id.toString(),
+        },
+      };
+
+      const userName = theUser.firstName;
+      const userEmail = theUser.email;
+
+      const authtoken = jwt.sign(payload, JWT_SECRET);
+      logger.info("User logged in successfully");
+      return res.status(200).json({ authtoken, userName, userEmail });
+    } else {
+      logger.error("User not found");
+      return res.status(404).json({ error: "User not found" });
+    }
+  } catch (e) {
+    logger.error(e);
+    return res
+      .status(500)
+      .json({ error: "Internal server error", details: e.message });
   }
 });
 
